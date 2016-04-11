624721d4ba38735c06baeffd5022943422390527
marcusyh <marcusyh@gmail.com>
2016-04-11 17:50:50 +0000

    update kraken

--- collect/ether/kraken.py
+++ collect/ether/kraken.py
@@ -1,37 +1,52 @@
 from data_agent import data_agent
 
-def get_price(flg):
-    url   = 'https://api.kraken.com/0/public/Ticker?pair=%s' %flag
-    rslt  = data_agent(url)
-
-    return float(rlst['result'][flag]['c'][0])
+def get_prices(flags):
+    url   = 'https://api.kraken.com/0/public/Ticker?pair=%s' %flags
+    return data_agent(url)['result']
+    
 
 def get_depth(flag):
     url  = 'https://api.kraken.com/0/public/Depth?pair=%s&count=100000000000' %flag
-
+    temp = data_agent(url)['result'][flag]
     bids = {}
     asks = {}
 
-    temp = data_agent(url)['result'][flag]
-    asks = temp['asks']
-    for x in rslt['bids']:
+    for x in temp['bids']:
         cur_price    = float(x[0])
         cur_source   = float(x[1])
         cur_btc      = cur_price * cur_source
 
         bids[cur_price] = cur_btc
 
+    for x in temp['asks']:
+        cur_price    = float(x[0])
+        cur_source   = float(x[1])
+
+        asks[cur_price] = cur_source
+
     return bids, asks
 
-def get_poloniex():
+
+def get_data():
     markets = {
-            'btc': 'xethxxbt', 
-            'eur': 'xethzeur', 
-            'usd': 'xethzusd', 
-            'cad': 'xethzcad', 
-            'gbp': 'xethzgbp', 
-            'jpy': 'xethzjpy'
+            'btc': 'XETHXXBT', 
+            'eur': 'XETHZEUR', 
+            'usd': 'XETHZUSD', 
+            'cad': 'XETHZCAD', 
+            'gbp': 'XETHZGBP', 
+            'jpy': 'XETHZJPY'
             }
-    price = get_price()
-    bids, asks = get_depth()
-    return price, bids, asks
+    result = {}
+
+    prices = get_prices(','.join(markets.itervalues()))
+    
+    for key, value in markets.iteritems():
+        bids, asks = get_depth(value)
+        result[key] = {
+                'price': float(prices[value]['c'][0]),
+                'volume': float(prices[value]['v'][1]),
+                'bids': bids,
+                'asks': asks
+                }
+    return result
+
